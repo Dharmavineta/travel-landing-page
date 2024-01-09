2c287f3dc8cb822d3f41d56ad9353a0bbe6de6ec
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-01-09 20:44:13 +0530

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "TravelGo",
   description: "Landing travel page",
+  icons: [{ url: "/travellogo.png", href: "/travellogo.png" }],
 };
 
 export default function RootLayout({
